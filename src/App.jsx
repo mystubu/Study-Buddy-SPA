4af7417aeee6a3c9ae0af52646377531a96ca81e
Marina Veldhuizen <marina.veldhuizen@student.fontys.nl>
2023-11-19 23:01:33 +0100

    feat(Marina): restructure directories, add eslint, add tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Test from './components/test/test'
+
+import './index.css';
+
+function App() {
+  return (
+    <>
+        <h1 className="text-3xl font-bold underline">Header</h1>
+      <Test></Test>
+    </>
+  );
+}
+
+export default App;
