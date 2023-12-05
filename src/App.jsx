d9df84528e82a69fff40186d777a46c2d437145d
daniel141002 <danielsinia@outlook.com>
2023-12-05 14:19:05 +0100

    login
    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './styles/App.css';
-import Chat from './views/chat/Chat';
+// import Chat from './views/chat/Chat';
 import Navigation from './views/navigation/Navigation';
 import Toolbar from './views/toolbar/Toolbar';
+import Login from './views/login/Login';
 
 function App() {
     return (
         <div className="App bg-background">
             <div className="grid grid-cols-12 h-screen text-primary">
                 <Navigation/>
-                <Chat />
+                <Login />
                 <Toolbar />
             </div>
         </div>
