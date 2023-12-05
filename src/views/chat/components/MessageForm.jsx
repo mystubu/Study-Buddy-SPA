4039b313f9c4180459c8b6c1a8b747e63203caf1
Mitchell <83954323+Quitzchell@users.noreply.github.com>
2023-12-05 19:20:47 +0100

    remove comment

--- src/views/chat/components/MessageForm.jsx
+++ src/views/chat/components/MessageForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types'; // Import PropTypes
+import PropTypes from 'prop-types';
 import { IoMdSend } from "react-icons/io";
 
 export default function MessageForm({ onSendMessage }) {
